727e024c52a16cd0369d717651785e1fa6e07509
PMayala <p.mayala@alustudent.com>
2024-05-14 01:12:26 -0700

    UPDATE: Defined a f(x) to create a new instance of BaseModel, save it, and print the id

--- console.py
+++ console.py
@@ -6,6 +6,18 @@ class HBNBCommand(cmd.Cmd):
     """Command interpreter for HBNB console."""
     
     prompt = "(hbnb) "
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it, and prints the id"""
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
     
     def do_quit(self, arg):
         """Quit command to exit the program."""
